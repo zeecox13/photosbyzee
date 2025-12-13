7191268c5387d5e95631eb4d2614ae2765c1a9b1
zeecox13 <zeecox13@gmail.com>
2025-12-12 23:29:07 -0600

    Remove incorrect vercel.json and simplify layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body>
+      <body className="antialiased">
         {children}
       </body>
     </html>
